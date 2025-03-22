484cfe5eaf1a2887a959cf0d012d90674233c87c
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-03-22 08:58:18 +0100

    Update LLDAP service file path (#3330)

--- install/lldap-install.sh
+++ install/lldap-install.sh
@@ -15,10 +15,11 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl
-$STD apt-get install -y sudo
-$STD apt-get install -y mc
-$STD apt-get install -y gpg
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc \
+  gpg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing lldap"
